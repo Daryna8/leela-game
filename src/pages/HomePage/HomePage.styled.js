9bef5131d1309c4f75913d8bb72a33f9287e3045
Daryna8 <142911382+Daryna8@users.noreply.github.com>
2024-05-15 18:45:59 +0300

    changed a bit styles

--- src/pages/HomePage/HomePage.styled.js
+++ src/pages/HomePage/HomePage.styled.js
@@ -2,10 +2,30 @@ import styled from "styled-components";
 
 export const StyledHomeContainer = styled.div`
   margin: 25px;
+
+  @media only screen and (min-width: 768px) {
+    margin: 45px;
+  }
+
+  @media only screen and (min-width: 1440px) {
+    margin: 85px;
+  }
 `;
 
 export const StyledTitle = styled.h2`
   color: white;
   font-size: 28px;
   line-height: 1.3;
+`;
+
+export const StyledInfo = styled.p`
+  font-size: 16px;
+
+  @media only screen and (min-width: 768px) {
+    width: 660px;
+  }
+
+  @media only screen and (min-width: 1440px) {
+    width: 1200px;
+  }
 `;
