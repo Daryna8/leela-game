0b2ce7217a81e1ce51859d577204d95684f81414
Daryna8 <142911382+Daryna8@users.noreply.github.com>
2024-05-15 18:35:15 +0300

    added some description

--- src/pages/HomePage/HomePage.styled.js
+++ src/pages/HomePage/HomePage.styled.js
@@ -1,7 +1,11 @@
 import styled from "styled-components";
 
+export const StyledHomeContainer = styled.div`
+  margin: 25px;
+`;
+
 export const StyledTitle = styled.h2`
   color: white;
-  font-size: 24px;
-  line-height: 1.5;
+  font-size: 28px;
+  line-height: 1.3;
 `;
